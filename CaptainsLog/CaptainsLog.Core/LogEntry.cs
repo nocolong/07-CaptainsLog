0f131767ca57981fdfb37489c3331ced3296eaf0
Greg McIntyre <nocolong@gmail.com>
2016-01-22 08:41:40 -0800

    Captains Log WPF

---
+++ CaptainsLog/CaptainsLog.Core/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainsLog.Core
+{
+    public class LogEntry
+    {
+        
+        public int Id { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ CaptainsLog/CaptainsLog/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CaptainsLog.Core;
+using System.Collections.ObjectModel;
+
+namespace CaptainsLog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<LogEntry> logEntries;
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            logEntries = new ObservableCollection<LogEntry>();
+
+            gridLogEntries.ItemsSource = logEntries;
+        }
+
+        private void buttonAddEntry_Click(object sender, RoutedEventArgs e)
+        {
+            string title = "Seperation of Concerns?";
+            string text = "Seperate a program into sections..";
+            DateTime date = DateTime.Now;
+            
+
+
+            LogEntry log = new Core.LogEntry();
+            log.Id = logEntries.Count + 1;
+            log.Title = title;
+            log.Text = text;
+            log.EntryDate = date;
+
+            logEntries.Add(log);
+
+
+        }
+
+        private void buttonDeleteLastEntry_Click(object sender, RoutedEventArgs e)
+        {
+            logEntries.Remove(logEntries[logEntries.Count - 1]);
+        }
+    }
+}
